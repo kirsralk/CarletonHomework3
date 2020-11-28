442548ecbc1de4575856a72357819b76cce40cd3
Kirsi Ralko <kirsiralko@gmail.com>
2020-11-28 13:44:13 -0600

    added pseudocode outline to script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt user for password criteria
+
+    // Criteria 1: Length of Password
+        // Validate: Must be between 8 to 128 characters
+
+    // Criteria 2: Uppercase Charcaters
+    
+    // Criteria 3: Lowercase Characters
+
+    // Criteria 4: Special Characters
+
+    // Validate: At least 1 character type selected
+        // If criteria not met, display an alert
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
