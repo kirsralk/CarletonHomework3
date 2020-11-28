b51551a26e9dd958670ab225986f047b10eb6bdf
Kirsi Ralko <kirsiralko@gmail.com>
2020-11-28 14:26:01 -0600

    prompts for password criteria added to index

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Prompt user for password criteria
 
-    // Criteria 1: Length of Password
-        // Validate: Must be between 8 to 128 characters
+// // Prompt user for password criteria
+function getPassCriteria (){
+    // alert("test test 2")
 
-    // Criteria 2: Uppercase Charcaters
-    
-    // Criteria 3: Lowercase Characters
+    // // Criteria 1: Length of Password
+    //     // Validate: Must be between 8 to 128 characters
+    var passLength = prompt("Please choose a password length between 8 to 128 characters.");
 
-    // Criteria 4: Special Characters
+    // // Criteria 2: Uppercase Charcaters
+    var passUpper = confirm("Click OK to include uppercase letters.");
 
-    // Validate: At least 1 character type selected
-        // If criteria not met, display an alert
+    // // Criteria 3: Lowercase Characters
+    var passLower = confirm("Click OK to include lowercase letters.");
+
+    // // Criteria 4: Special Characters
+    var passSpecial = confirm("Click OK to include special characters.");
+
+    // // Validate: At least 1 character type selected
+    //     // If criteria not met, display an alert
+}
+
+// // Generate password using criteria
+// function generatePassword() {
+//     var passCriteria = getpassCriteria();
+// }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    getPassCriteria();
+    // var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    // passwordText.value = password;
+    // alert("test test");
 }
 
 // Add event listener to generate button
